9c3441a0a2325a469fc29c21c32adab240c506f3
Cesar <cesar.vargas@rookmotion.com>
2021-02-01 14:19:41 -0600

    Added new library for circle an image view and backbutton logic added

--- app/src/main/java/com/example/socialmediagamer/RegisterActivity.java
+++ app/src/main/java/com/example/socialmediagamer/RegisterActivity.java
@@ -3,12 +3,27 @@ package com.example.socialmediagamer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        mCircleImageViewBack = findViewById(R.id.circle_back);
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
