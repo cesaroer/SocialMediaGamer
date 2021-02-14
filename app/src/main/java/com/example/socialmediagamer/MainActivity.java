2bbf196f76a510e077eb9b872a6a966760833509
cesaroer <cesarvarcard@gmail.com>
2021-02-14 11:56:23 -0600

    Added login with firebaseAuth and HomeActivity

--- app/src/main/java/com/example/socialmediagamer/MainActivity.java
+++ app/src/main/java/com/example/socialmediagamer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.socialmediagamer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -8,14 +9,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mTextViewRegister;
     TextInputEditText mTextInputEmail, mTextInputPassword;
     AppCompatButton mBtnLogin;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         mTextInputEmail     = findViewById(R.id.textInputEmail);
         mTextInputPassword  = findViewById(R.id.textInputPassword);
         mBtnLogin = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
     private void login() {
         String email = mTextInputEmail.getText().toString();
         String password = mTextInputPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else {
+
+                    Toast.makeText(MainActivity.this,"email o contraseña no son correctas" , Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         Log.d("CAMPO","email: " + email);
         Log.d("CAMPO","password: " + password);
