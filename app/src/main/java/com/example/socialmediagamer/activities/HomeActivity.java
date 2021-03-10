11507bd8576be6770cdc1be669256895f41b144e
cesaroer <cesarvarcard@gmail.com>
2021-03-09 21:08:58 -0600

    Added bottom navigation view with 3 new fragments

--- app/src/main/java/com/example/socialmediagamer/activities/HomeActivity.java
+++ app/src/main/java/com/example/socialmediagamer/activities/HomeActivity.java
@@ -1,16 +1,65 @@
 package com.example.socialmediagamer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.socialmediagamer.R;
+import com.example.socialmediagamer.fragments.ChatsFragment;
+import com.example.socialmediagamer.fragments.FiltersFragment;
+import com.example.socialmediagamer.fragments.HomeFragment;
+import com.example.socialmediagamer.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new HomeFragment());
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    if (item.getItemId() == R.id.itemHome) {
+                        //FRAGMENT HOME
+                        openFragment(new HomeFragment());
+                    }else if (item.getItemId() == R.id.itemChat){
+                        //FRAGMENT CHATS
+                        openFragment(new ChatsFragment());
+
+                    }else if (item.getItemId() == R.id.itemFilters){
+                        //FRAGMENT FILTER
+                        openFragment(new FiltersFragment());
+
+                    }else if (item.getItemId() == R.id.itemProfile){
+                        //FRAGMENT PROFILE
+                        openFragment(new ProfileFragment());
+
+                    }
+                    return true;
+                }
+            };
+
+
 }
